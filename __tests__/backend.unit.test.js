aab8a87aabd1dae0ec752ffc9d4de9f4ec5efb63
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-11-02 14:58:56 -0700

    Add database, server, and mock enzyme tests

---
+++ __tests__/backend.unit.test.js
@@ -0,0 +1,41 @@
+const Shoe = require('../db/shoeTitle');
+const seed = require('../db/seeder');
+
+test('properly empties the database', (done) => {
+  Shoe.deleteMany({}, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    Shoe.countDocuments({}, (error, count) => {
+      expect(count).toEqual(0);
+      done();
+    });
+  });
+});
+
+test('properly adds documents to the database', (done) => {
+  const shoes = [{ shoeID: 11111, imageUrls: ['test1.jpg', 'test2.jpg'] }, { shoeID: 22222, imageUrls: ['test3.jpg', 'test4.jpg', 'test5.jpg'] }];
+  Shoe.insertMany(shoes, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    Shoe.countDocuments({}, (error, count) => {
+      expect(count).toBe(2);
+      done();
+    });
+  });
+});
+
+describe('Check for proper seeding of database', () => {
+  it('seeded the database with 122 shoes', (done) => {
+    seed(() => {
+      Shoe.countDocuments({}, (error, count) => {
+        if (error) {
+          console.log(error);
+        }
+        expect(count).toBe(122);
+        done();
+      });
+    });
+  });
+});
--- src/components/component.unit.test.js
+++ src/components/component.unit.test.js
@@ -12,8 +12,10 @@ import ColorsRow from './ColorsRow';
 import Name from './Name';
 
 const sinon = require('sinon');
+const mockAxios = require('../../__mocks__/axios');
 
 configure({ adapter: new Adapter() });
+
 
 describe('[App] Component Unit Tests', () => {
   const wrapper1 = shallow(<App />);
@@ -26,7 +28,6 @@ describe('[App] Component Unit Tests', () => {
     mount(<App shoeID="AV1200-001" />);
     expect(App.prototype.componentDidMount.callCount).toBe(1);
   });
-
 });
 
 describe('[ColorsGrid] Component Unit Tests', () => {
@@ -35,6 +36,13 @@ describe('[ColorsGrid] Component Unit Tests', () => {
     expect(wrapper1.exists()).toBeTruthy();
   });
 
+  it('should correctly update state when getImageLinks function called', async () => {
+    window.axios = mockAxios;
+    const instance = wrapper1.instance();
+    expect(wrapper1.state('images').length).toBe(0);
+    await instance.getImageLinks(['AV1200-001']);
+    expect(wrapper1.state('images').length).toBe(1);
+  });
 });
 
 describe('[ColorsRow] Component Unit Tests', () => {
