5a6adec70d0f59a9321064346ece602757b55a50
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-11-02 12:23:11 -0700

    Write basic rendering tests for module components

--- src/components/component.unit.test.js
+++ src/components/component.unit.test.js
@@ -1,7 +1,7 @@
 /* eslint react/jsx-filename-extension: [0] */
 
 import {
-  shallow, mount, render, configure,
+  shallow, mount, configure,
 } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
@@ -11,6 +11,8 @@ import ColorsGrid from './ColorsGrid';
 import ColorsRow from './ColorsRow';
 import Name from './Name';
 
+const sinon = require('sinon');
+
 configure({ adapter: new Adapter() });
 
 describe('[App] Component Unit Tests', () => {
@@ -18,24 +20,32 @@ describe('[App] Component Unit Tests', () => {
   it('should exist', () => {
     expect(wrapper1.exists()).toBeTruthy();
   });
+
+  it('should call componentDidMount when component is mounted', () => {
+    sinon.spy(App.prototype, 'componentDidMount');
+    mount(<App shoeID="AV1200-001" />);
+    expect(App.prototype.componentDidMount.callCount).toBe(1);
+  });
+
 });
 
 describe('[ColorsGrid] Component Unit Tests', () => {
-  const wrapper1 = shallow(<App />);
+  const wrapper1 = shallow(<ColorsGrid />);
   it('should exist', () => {
     expect(wrapper1.exists()).toBeTruthy();
   });
+
 });
 
 describe('[ColorsRow] Component Unit Tests', () => {
-  const wrapper1 = shallow(<App />);
+  const wrapper1 = shallow(<ColorsRow />);
   it('should exist', () => {
     expect(wrapper1.exists()).toBeTruthy();
   });
 });
 
 describe('[Name] Component Unit Tests', () => {
-  const wrapper1 = shallow(<App />);
+  const wrapper1 = shallow(<Name />);
   it('should exist', () => {
     expect(wrapper1.exists()).toBeTruthy();
   });
